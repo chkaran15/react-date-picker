f335dd38ca668a30099dcee948b8a31d82cf6cca
Karan Chaudhary <tharukaran930@gmail.com>
2024-12-22 02:05:16 +0545

    chore(feat): commit message check

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import pkg from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,9 @@ export default defineConfig({
 		alias: {
 			'@': path.resolve(__dirname, './src'),
 		},
+	},
+	optimizeDeps: {
+		include: Object.keys(pkg.dependencies),
 	},
 	build: {
 		rollupOptions: {
